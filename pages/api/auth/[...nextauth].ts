ee431b3349dbb075f7efa4ecabaf06d3751f2f2d
Daniel Oppenlander <daniel@oppenlander.net>
2023-01-02 15:57:26 -0800

    added prisma adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,13 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-export const authOptions = {
-  // Configure one or more authentication providers
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import prisma from "../../../lib/prisma";
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
-};
-export default NextAuth(authOptions);
+});
