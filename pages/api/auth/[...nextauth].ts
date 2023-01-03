e15379b28448dccad2955576e3ccc2fab54f0f08
Daniel Oppenlander <daniel@oppenlander.net>
2023-01-02 16:35:06 -0800

    updated prisma schema

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,8 +2,19 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prisma";
+
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "database",
+
+    // 30 Days
+    maxAge: 30 * 24 * 60 * 60,
+
+    // Refresh session every 24 hours
+    updateAge: 24 * 60 * 60
+  },
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
